e6b1d96a478d6f16f143bcb59fe61542cb83bdec
Borkla <50032455+Borkla@users.noreply.github.com>
2020-04-16 17:02:23 +0200

    platforms that are moving are now working properly

--- 3d shit/Assets/Scripts/PlatformMoving.cs
+++ 3d shit/Assets/Scripts/PlatformMoving.cs
@@ -2,53 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMoving : MonoBehaviour
+public class PlatformMoving : MovingPlatformStateMachine
 {
-    public float lengthToMove = 15f;
-    public Vector3 startposition;
+    public Vector3 startPosition;
+    public Vector3 endPositioon;
+    public Vector3 targetPosition;
     public bool frozen = false;
+    public string currentState;
     public float timeTillDefrosted;
     public bool tester = false;
     public float freezeTestTime = 5f;
-    public Vector3 Direction = new Vector3(0,1,0);
     public float platformSpeed = 5f;
-    // Start is called before the first frame update
+    public float movementTimer = 10f;
+    public Vector3 velocity;
+
     void Start()
     {
-        startposition = transform.position;
+        SetState(new MovingState(this));
     }
+}
 
-    // Update is called once per frame
-    void Update()
+public abstract class BasePlatformState
+{
+    protected PlatformMoving PlatformMoving;
+
+    public BasePlatformState(PlatformMoving platformMoving)
     {
-        if (!frozen)
-        {
-            transform.position += Direction.normalized * platformSpeed * Time.deltaTime;
-        }
-
-        if ((transform.position - startposition).magnitude > lengthToMove)
-        {
-            Direction = -Direction;
-            Debug.Log("turned around now");
-            Debug.Log(transform.position.magnitude + " transform magnitude  "+ startposition.magnitude +" startposition magnitude");
-        }
-
-        if (timeTillDefrosted > Time.time && frozen)
-        {
-            frozen = false;
-            tester = false;
-        }
-
-        // remove after the freeze function is implemented with player
-        // and reomove the other parts connected to tester
-        if (tester)
-        {
-            Freeze(freezeTestTime);
-        }
+        PlatformMoving = platformMoving;
     }
-    public void Freeze(float freezeTime)
+    public virtual void Start()
     {
-        timeTillDefrosted = Time.time + freezeTime;
-        frozen = true;
+        return;
     }
+    public virtual void Movement()
+    {
+        return;
+    }
+    public virtual void Freeze()
+    {
+        return;
+    }
+}
+
+public abstract class MovingPlatformStateMachine : MonoBehaviour
+{
+    protected BasePlatformState thisState;
+    public void SetState(BasePlatformState state)
+    {
+        Debug.Log("tog mig in i statemachinen");
+        thisState = state;
+        thisState.Start();
+    }
+    private void Update()
+    {
+        thisState.Movement();
+    }
+}
+
+public class MovingState : BasePlatformState
+{
+    public MovingState(PlatformMoving platformMoving) : base(platformMoving)
+    {
+    }
+    public override void Start()
+    {
+        if (PlatformMoving.startPosition.magnitude == 0)
+        {
+            Debug.Log("tog mig in i Start i movementState Ska bara hända en gång");
+            PlatformMoving.startPosition = PlatformMoving.transform.position;
+            PlatformMoving.targetPosition = PlatformMoving.endPositioon;
+        }
+        
+    }
+    public override void Movement()
+    {
+        PlatformMoving.velocity = (PlatformMoving.targetPosition - PlatformMoving.transform.position).normalized *PlatformMoving.platformSpeed *Time.deltaTime;
+        Debug.Log("Tog mig in i movementState Movement");
+        PlatformMoving.transform.position = Vector3.MoveTowards(PlatformMoving.transform.position, PlatformMoving.targetPosition, PlatformMoving.platformSpeed * Time.deltaTime);
+
+        if ((PlatformMoving.transform.position - PlatformMoving.targetPosition).magnitude == 0)
+        {
+            if (PlatformMoving.targetPosition == PlatformMoving.endPositioon)
+            {
+                PlatformMoving.targetPosition = PlatformMoving.startPosition;
+            }
+            else if (PlatformMoving.targetPosition == PlatformMoving.startPosition)
+            {
+                PlatformMoving.targetPosition = PlatformMoving.endPositioon;
+            }
+
+        }
+        if (PlatformMoving.frozen)
+        {
+            PlatformMoving.SetState(new FreezeState(PlatformMoving));
+        }
+    }
+}
+
+public class FreezeState : BasePlatformState
+{
+    public FreezeState(PlatformMoving platformMoving) : base(platformMoving)
+    {
+    }
+    public override void Start()
+    {
+        Debug.Log("tog mig in i Freeze state");
+        PlatformMoving.timeTillDefrosted = Time.time + PlatformMoving.freezeTestTime;
+    }
+    public override void Movement()
+    {
+            if (Time.time > PlatformMoving.timeTillDefrosted)
+            {
+                PlatformMoving.frozen = false;
+                PlatformMoving.SetState(new MovingState(PlatformMoving));
+            }
+    }
+
 }
